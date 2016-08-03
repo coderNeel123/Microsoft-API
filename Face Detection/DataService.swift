6c56bde70b7b33458eb7ce14f3df409926741436
Neel Khattri <Neel@neels-mbp.attlocal.net>
2016-08-03 02:40:32 -0700

    Finished Microsoft API

---
+++ Face Detection/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  Face Detection
+//
+//  Created by Neel Khattri on 8/1/16.
+//  Copyright © 2016 SimpleStuff. All rights reserved.
+//
+
+import Foundation
---
+++ Face Detection/ProduceImage.swift
@@ -0,0 +1,9 @@
+//
+//  ProduceImage.swift
+//  Face Detection
+//
+//  Created by Neel Khattri on 8/2/16.
+//  Copyright © 2016 SimpleStuff. All rights reserved.
+//
+
+import Foundation
